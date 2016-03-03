bc3fe3fc8242cb704bf62a2974e4bc08f7b9f6dc
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2016-03-03 19:30:16 +0100

    Funciones compilan

--- Src/funcionesDaniel.cc
+++ Src/funcionesDaniel.cc
@@ -1,11 +1,14 @@
+#include <iostream>
 #include "cabecera.h"
+using namespace std;
+
 // Maneja los argumentos y saca del programa si la sintaxis es erronea
 int comprobarArgumentos(int argc){
 	int toRet = 0;
 
 	if (argc != 4){
 		cout << "Error. Se debe indicar el puerto del servidor y la ruta al archivo de configuracion\n\r";
-		cout << "Sintaxis: http_server [puerto] [-c fichero_configuración]\n\r";
+		cout << "Sintaxis: http_server [puerto] [-c fichero_configuraciï¿½n]\n\r";
 		toRet = 1;
 	}
 
