ecd98d082faf8b5eec6973a62daa7212e0066713
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2016-03-03 18:31:37 +0100

    ManageArguments hecho.

--- Src/MyHTTPServer.cc
+++ Src/MyHTTPServer.cc
@@ -2,31 +2,48 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){ 
-/* Hay que pasar por comando el puerto y el archivo con la orden */
 
-/* Comprobamos los argumentos */
-	if(argc != 3){ 
-		fprintf(stderr, "Error. Debe indicar el puerto del servidor y el archivo de configuracion\n\r");
-		fprintf(stderr, "Sintaxis: 3 <puerto> <-c archivo de configuracion>\n\r");
-		return 1;
+// Maneja los argumentos y saca del programa si la sintaxis es erronea
+int comprobarArgumentos(int argc){
+	int toRet = 0;
+
+	if (argc != 4){
+		cout << "Error. Se debe indicar el puerto del servidor y la ruta al archivo de configuracion\n\r";
+		cout << "Sintaxis: http_server [puerto] [-c fichero_configuración]\n\r";
+		toRet = 1;
 	}
 
-/* Leer el archivo de configuracion del servidor */
+	return toRet;
+}
 
-	/* DocumentRoot que nos indique donde estan los archivos */
-	/* Maxclients que será el número maximo de procesos que lanzará el servidor */
-	/* Puerto en el que escuchará el servidor */
-	/* Página por defecto que se envia si no se indica otra */
 
-/* Declaramos las variables necesarias */
+int main(int argc, char *argv[]){ 
+
+	if(comprobarArgumentos(argc) == 1) return 1;
+	return 0;
+
+}
+
+
+
+
+/*
+
+// Leer el archivo de configuracion del servidor 
+
+	 //DocumentRoot que nos indique donde estan los archivos 
+	 //Maxclients que será el número maximo de procesos que lanzará el servidor 
+	 //Puerto en el que escuchará el servidor 
+	 //Página por defecto que se envia si no se indica otra 
+
+// Declaramos las variables necesarias 
 	
 	int Socket;
 	int Puerto = argv[1];
 	struct Direccion;
 
 
-/* Abrimos el socket */
+// Abrimos el socket 
 	
 	Socket = socket(AF_INET, SOCK_STREAM, 0);
 	if(Socket == -1){
@@ -36,7 +53,7 @@ int main(int argc, char *argv[]){
 		printf("Socket abierto\n\r");
 	}
 
-/* Enlazamos una IP y un puerto al socket */
+// Enlazamos una IP y un puerto al socket 
 
 	Direccion.sin_family = AF_INET;
 	Direccion.sin_port = htons(Puerto);
@@ -51,7 +68,7 @@ int main(int argc, char *argv[]){
 		fprintf("Puerto de escucha establecido\n\r");
 	}
 
-/* Preparamos el servidor para empezar a escuchar */
+// Preparamos el servidor para empezar a escuchar 
 	
 	if(listen(Socket, Maxclients) == -1){
 		fprintf(stderr, "Error preparando el servidor\n\r");
@@ -62,8 +79,6 @@ int main(int argc, char *argv[]){
 		fprintf("Servidor preparado correctamente\n\r");
 	}
 
-/* Esperar conexiones */
+// Esperar conexiones 
 
-
-	return 0;
-}
+*/
