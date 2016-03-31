496a38982d26967529bcd7609058b4ac91c4f3be
angelnm <angel.nm@hotmail.es>
2016-03-31 15:19:20 +0200

    Leer método y Uri

--- Src/cabecera.h
+++ Src/cabecera.h
@@ -7,3 +7,5 @@ int inicializarSocket();
 void leerDatos(string archivo, string* DocumentRoot, int* Maxclients, int* Puerto, string* Pagina);
 int esperarConexiones(int maxClients, string documentRoot, string pagina, int s);
 void finalizar(int senyal);
+int ver_metodo(char mensaje[]);
+string ver_uri(char mensaje[]);
