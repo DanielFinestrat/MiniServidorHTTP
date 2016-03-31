0e008c69499d551831fbcf849ae41980b137fd21
DanielFinestrat <danielfinestratmartinez@hotmail.com>
2016-03-31 18:45:19 +0200

    Cabeceras actualizadas, mensaje predeterminado eliminado

--- Src/MyHTTPServer.cc
+++ Src/MyHTTPServer.cc
@@ -16,17 +16,16 @@ using namespace std;
 
 int miSocket; //Nuestro socket, variable global
 
-void finalizar (int senyal)
-{
+void finalizar (int senyal){
     printf("Recibida la señal de fin (cntr-C)\n\r");
-    close(miSocket); /* cerrar para que accept termine con un error y salir del bucle principal */
+    close(miSocket); //Cerrar para que accept termine con un error y salir del bucle principal
 }
 
 int main(int argc, char *argv[]){
 
-    char mensaje[1024];
-    char respuesta[] = "HTTP/1.0 200 OK\nConnection: keep-alive\nContent-Type: text/html; charset=UTF-8;\n\n<html><head><title>Index</title></head><body><h1>Mi titulo</h1><p>Mi <b>indice</b></p><ul><li style='color:red;'>Rojo</li><li style='color:blue;'>Azul</li><li style='color:green;'>Verde</li></body></html>";
-    int n, enviados, recibidos, s2, proceso;
+    	char mensaje[1024];
+    	char respuesta[] = "";
+    	int n, enviados, recibidos, s2, proceso;
 	string documentRoot;    //Nos indica donde estan los archivos
 	string rutaConf = "";	//Nos indica donde esta el fichero de configuracion
 	string pagina;          //Pagina que por defecto que se envia si no se indica otra
@@ -45,102 +44,76 @@ int main(int argc, char *argv[]){
 	miSocket = inicializarSocket(); if(miSocket == -1) return 1;
 	
 	// Enlazamos una IP y un puerto al socket
-    unsigned int long_Cliente;
-    struct sockaddr_in Direccion, Cliente;
-    Direccion.sin_family = AF_INET;
-    Direccion.sin_port = htons(puerto);
-    Direccion.sin_addr.s_addr = INADDR_ANY;
+    	unsigned int long_Cliente;
+   	struct sockaddr_in Direccion, Cliente;
+    	Direccion.sin_family = AF_INET;
+    	Direccion.sin_port = htons(puerto);
+    	Direccion.sin_addr.s_addr = INADDR_ANY;
 
 	if(bind(miSocket, (struct sockaddr *)&Direccion, sizeof (Direccion)) == -1){
 		cout << "Error. No se puede asociar el puerto al socket\n\r" ;
-		close(miSocket);
-		return(1);
+		close(miSocket); return(1);
 	}else cout <<"Puerto de escucha establecido\n\r";
 
 
 	// Preparamos el servidor para empezar a escuchar
 	if(listen(miSocket, maxclients) == -1){
 		cout << "Error preparando el servidor\n\r";
-		close(miSocket);
-		return(1);
+		close(miSocket); return(1);
 	}
 	else cout << "Servidor preparado correctamente\n\r";
 
-    signal(SIGINT, finalizar);
+    	signal(SIGINT, finalizar);
     
-    while (1){
-        cout<< "Esperando conexión en el puerto " << puerto <<endl;
-        long_Cliente = sizeof (Cliente);
-        s2 = accept (miSocket, (struct sockaddr *)&Cliente, &long_Cliente);
-        /* s2 es el socket para comunicarse con el cliente */
-        /* s puede seguir siendo usado para comunicarse con otros clientes */
-        if (s2 == -1)
-        {
-            break; /* salir del bucle */
-        }
-        /* crear un nuevo proceso para que se pueda atender varias peticiones en paralelo */
-        proceso = fork();
-        if (proceso == -1) exit(1);
-        if (proceso == 0) /* soy el hijo */
-        {
-            close(miSocket); /* el hijo no necesita el socket general */
+    	while (1){
+        	cout<< "Esperando conexión en el puerto " << puerto <<endl;
+        	long_Cliente = sizeof (Cliente);
+        	s2 = accept (miSocket, (struct sockaddr *)&Cliente, &long_Cliente);
+        	
+		/* s2 es el socket para comunicarse con el cliente
+        	   s puede seguir siendo usado para comunicarse con otros clientes */
+
+        	if (s2 == -1){ break; } //Salir del bucle
+       		proceso = fork(); //crear un nuevo proceso para que se pueda atender varias peticiones en paralelo
+        	if (proceso == -1) exit(1);
+        	if (proceso == 0){ //Soy el hijo
+            		close(miSocket); //El hijo no necesita el socket general
             
-            /**** Paso 5: Leer el mensaje ****/
-            
-            n = sizeof(mensaje);
-            recibidos = read(s2, mensaje, n);
-            if (recibidos == -1)
-            {
-                cout<< "Error leyendo el mensaje" <<endl;
-                exit(1);
-            }
-            mensaje[recibidos] = '\0'; /* pongo el final de cadena */
+            		/**** Paso 5: Leer el mensaje ****/
+		    	n = sizeof(mensaje);
+		   	recibidos = read(s2, mensaje, n);
+		    	if (recibidos == -1){ cout<< "Error leyendo el mensaje" <<endl; exit(1); }
+		    	mensaje[recibidos] = '\0'; // pongo el final de cadena
             
             
-            //EMPEZAMOS A TRATAR EL MENSAJE QUE RECIBIMOS
-            cout<<"Mensaje ["<<recibidos<<"] : "<< mensaje <<endl;
-          
-	    int metodo; /* 0=Método Erroneo 1=GET 2=HEAD 3=PUT 4=DELETE */
-	    metodo = ver_metodo(mensaje);
+			//EMPEZAMOS A TRATAR EL MENSAJE QUE RECIBIMOS
+		    	cout<<"Mensaje ["<<recibidos<<"] : "<< mensaje <<endl;
+		  
+		    	int metodo; /* 0=Método Erroneo 1=GET 2=HEAD 3=PUT 4=DELETE */
+		    	metodo = ver_metodo(mensaje);
 
 	
-	    if(metodo == 0){
-		//Error 405
-	    }
-	    else{
-	    string uri = ver_uri(mensaje);
+		    	if(metodo == 0){ /*Error 405*/ }
+		    	else{ string uri = ver_uri(mensaje); cout<< uri <<endl; } //No se comprueba si la uri es valida
 		
-	//No se comprueba si la uri es valida
-	    cout<< uri <<endl;
 
-	    }	
-	   
             
-            /**** Paso 6: Enviar respuesta ****/
+            		/**** Paso 6: Enviar respuesta ****/
+			n = strlen(respuesta);
+			printf("Enviar respuesta [%d bytes]: %s\n\r", n, respuesta);
+			enviados = write(s2, respuesta, n);
+			if (enviados == -1 || enviados < n){
+				fprintf(stderr, "Error enviando la respuesta (%d)\n\r",enviados);
+				close(miSocket); return 1;
+			}
+            		printf("Respuesta enviada\n\r");
             
-            n = strlen(respuesta);
-            printf("Enviar respuesta [%d bytes]: %s\n\r", n, respuesta);
-            enviados = write(s2, respuesta, n);
-            if (enviados == -1 || enviados < n)
-            {
-                fprintf(stderr, "Error enviando la respuesta (%d)\n\r",enviados);
-                close(miSocket);
-                return 1;
-            }
-            
-            printf("Respuesta enviada\n\r");
-            
-            close(s2);
-            exit(0); /* el hijo ya no tiene que hacer nada */
+            		close(s2);
+           		exit(0); //El hijo ya no tiene que hacer nada
         }
-        else /* soy el padre */
-        {
-            close(s2); /* el padre no usa esta conexión */
-        }
-        
+        else{ close(s2); } //Soy el padre y no uso esta conexión
     }
-    
 	close(miSocket);
-    cout<< "Socket cerrado" <<endl;
+	cout<< "Socket cerrado" <<endl;
 	return 0;
 }
--- Src/cabecera.h
+++ Src/cabecera.h
@@ -5,7 +5,5 @@ using namespace std;
 int manageArguments(int argc, char *argv[], string &rutaConf, int &puerto);
 int inicializarSocket();
 void leerDatos(string archivo, string* DocumentRoot, int* Maxclients, int* Puerto, string* Pagina);
-int esperarConexiones(int maxClients, string documentRoot, string pagina, int s);
-void finalizar(int senyal);
 int ver_metodo(char mensaje[]);
 string ver_uri(char mensaje[]);
