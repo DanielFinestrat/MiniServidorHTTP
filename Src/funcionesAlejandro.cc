83829864b5406b00227c5d558e1e2a848c2f5634
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2016-04-13 13:14:44 +0200

    Update funcionesAlejandro.cc

--- Src/funcionesAlejandro.cc
+++ Src/funcionesAlejandro.cc
@@ -7,3 +7,50 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <sstream>
+
+// recibe el metodo y la uri para responder acordemente
+string tratarPeticion(string root,int metodo,string uri) {
+	string doc = "HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nServer: MyHTTPServer/0.1\nContent-Lenght: ";
+	switch (metodo) {
+		case 1: { // get
+			string fich = parsearFicheroGET(root + uri);
+			string len = intToStr(fich.length());
+			doc+=len;
+			doc+="\n\n";			
+			doc += fich;
+			break;
+		}
+		case 2: // head
+		{
+			string fich = parsearFicheroGET(root + uri);
+			string len = intToStr(fich.length());
+			doc+=len;
+			doc+="\n\n";
+			break;
+		}
+		case 3: // put
+		break;
+		case 4: // delete
+		break;
+	}
+	return doc;
+}
+// Devuelve un STRING que contiene un fichero
+string parsearFicheroGET(string ruta) {
+
+	string toRet = "";
+	string temp = "";
+	string modRuta = ruta;
+
+    ifstream fe(modRuta.c_str());
+	while(!fe.eof()){ getline(fe, temp); toRet += temp; }
+	fe.close();
+
+	return(toRet);
+}
+
